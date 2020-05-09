83f375be14690b89f11b9c71579c1bdc16e55b6e
hchiam <hchiam@gmail.com>
2020-05-09 19:13:22 -0400

    make more robust

--- codepen-full-page-link.js
+++ codepen-full-page-link.js
@@ -3,12 +3,17 @@
 function _showRedirectLinkAsNeeded(codePenIdentifier) {
   const thisUrl = document.URL;
   if (thisUrl.indexOf("/full") === -1) {
-    const fullPageURL = "https://codepen.io/hchiam/full/" + codePenIdentifier;
-    document.getElementById("gofullpage").innerHTML = "Full page view: ";
+    const fullPageURL = "https://codepen.io/hchiam/full/" + "asdf";
+    let gofullpage = document.getElementById("gofullpage");
+    if (!gofullpage) {
+      gofullpage = document.createElement("p");
+      document.body.prepend(gofullpage);
+    }
+    gofullpage.innerHTML = "Full page view: ";
     const aTag = document.createElement("a");
     aTag.setAttribute("href", fullPageURL);
     aTag.setAttribute("target", "_blank"); // open in new window
     aTag.innerHTML = "here";
-    document.getElementById("gofullpage").appendChild(aTag);
+    gofullpage.appendChild(aTag);
   }
 }
